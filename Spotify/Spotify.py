e21d5109e3995c1f07ec1d3ad27ed99678fe3c6f
Unknown <gameboy95thang@gmail.com>
2020-02-19 16:07:11 -0600

    display songs on search

--- Spotify/Spotify.py
+++ Spotify/Spotify.py
@@ -1,6 +1,6 @@
 import spotipy
 import spotipy.util as util
-
+import json
 
 def show_tracks(tracks):
     for i, item in enumerate(tracks['items']):
@@ -9,19 +9,23 @@ def show_tracks(tracks):
         song = track['name']
         print("%s - %s" % (artist, song))
 
+def show_songs(tracks):
+    for i, item in enumerate(tracks['items']):
+        song = item["name"]
+        artist = item["artists"][0]["name"]
+        ext_url = item["external_urls"] #link to song
+        id = item["id"]
+        print(f'{i}: ', song, "by", artist)
+
 
 def display_playlist(token, link):
-    # link = input('Enter the playlist url: ')
-    # link = 'https://open.spotify.com/playlist/6SXzRD2I9kBN4iFQt4Rn4B'
     linkArr = link.split('/')
     playlist_id = linkArr[-1].strip()
-    endpoint = f'https://api.spotify.com/v1/playlists/{playlist_id}'
-    authCode = "BQC86IUaQt8bUYyedd1GDOexhnZ0PoDHxLgm0nH1JIbD5SYvCvsa8S8xIK40808smpaIKrj7b4lb4ohlrRk9BQ8XlRkJ"
 
     if token:
         sp = spotipy.Spotify(auth=token)
-        list = sp.playlist(playlist_id, fields="tracks,next")
-        song = list['tracks']
+        songs = sp.playlist(playlist_id, fields="tracks,next")
+        song = songs['tracks']
         show_tracks(song)
         while song['next']:
             song = sp.next(song)
@@ -34,22 +38,26 @@ def searchTracks(token, track):
     if token:
         sp = spotipy.Spotify(auth=token)
         song = sp.search(q=track, type='track')
-        print(song)
+        tracks = song['tracks']
+        show_songs(tracks)
     else:
         print("Can't get token")
 
 
 if __name__ == '__main__':
-    user = '1214261898'
-    clientId = '6fc4feaa2fce44ebacf339ab0ea62757'
-    secret = '9851d5c181684612acf523bafde3c66e'
+    with open('./Secrets/secrets.json') as f:
+        data = json.load(f)
+
+    user = data['user']
+    clientId = data['clientId']
+    secret = data['secret']
 
     token = util.prompt_for_user_token(user,
                                        scope='playlist-modify-private,playlist-modify-public',
                                        client_id=clientId,
                                        client_secret=secret,
                                        redirect_uri='https://localhost:8080')
-
+    
     running = True
     choice = input("Welcome to Media Companion, what would you like to do? "
                    "\n1) Search for a song"
@@ -64,12 +72,16 @@ if __name__ == '__main__':
         elif choice == '2':
             link = input("Enter your playlist URL: ")
             display_playlist(token, link)
-        else:
+        elif choice == 'q':
             print('Thanks goodbye')
             running = False
+            exit
+        else:
+            print('Invalid choice enter again: ')
 
-        choice = input("\nWhat would you like to do? "
-                       "\n1) Search for a song"
-                       "\n2) Load a playlist"
-                       "\nq) Quit"
-                       "\nChoice: ")
+        if choice != 'q':
+            choice = input("\nWhat would you like to do? "
+                           "\n1) Search for a song"
+                           "\n2) Load a playlist"
+                           "\nq) Quit"
+                           "\nChoice: ")
