a3e80bd04bd652fd48703f73176ee9bc1fdd87a5
Andres Munoz Ornelas <munoz24@uwm.edu>
2020-04-29 16:08:27 -0500

    Pickle

--- Youtube/yt.py
+++ Youtube/yt.py
@@ -17,8 +17,8 @@ def main():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists('id564.pickle'):
+        with open('id564.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -29,24 +29,13 @@ def main():
                 'secret/client_secret.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open('id564.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('youtube', 'v3', credentials=creds)
 
     # Call the People API
     print('List 10 connection names')
-    results = service.people().connections().list(
-        resourceName='people/me',
-        pageSize=10,
-        personFields='names,emailAddresses').execute()
-    connections = results.get('connections', [])
-
-    for person in connections:
-        names = person.get('names', [])
-        if names:
-            name = names[0].get('displayName')
-            print(name)
 
 
 if __name__ == '__main__':
