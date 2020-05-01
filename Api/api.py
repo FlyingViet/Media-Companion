2ad77cb9aee4d999c7e26bb22bbbaae3e9de619a
Andres Munoz Ornelas <munoz24@uwm.edu>
2020-05-01 13:52:09 -0500

    Reverted back the Redirect

--- Api/api.py
+++ Api/api.py
@@ -25,13 +25,15 @@ secret = secrets['secret']
 
 youtube = None
 
+
 class Database:
     def __init__(self):
         host = data['hostInfo']
         user = data['userInfo']
         password = data['passwordInfo']
         db = data['databaseInfo']
-        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.DictCursor)
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db,
+                                   cursorclass=pymysql.cursors.DictCursor)
         self.cur = self.con.cursor()
 
     def list_users(self):
@@ -43,10 +45,14 @@ class Database:
         self.cur.execute("INSERT INTO Users(email, password) VALUES(%s, %s)", (email, password))
 
     def insert_song(self, songName, songArtist, spotifyId, SpotifyUrl, uri):
-        self.cur.execute("INSERT IGNORE INTO Songs(SongName, SongArtist, SpotifyID, SpotifyUrl, SpotifyUri) VALUES( %s, %s, %s, %s, %s)", (songName, songArtist, spotifyId, SpotifyUrl, uri))
+        self.cur.execute(
+            "INSERT IGNORE INTO Songs(SongName, SongArtist, SpotifyID, SpotifyUrl, SpotifyUri) VALUES( %s, %s, %s, %s, %s)",
+            (songName, songArtist, spotifyId, SpotifyUrl, uri))
 
     def insert_multiple_songs(self, songList):
-        self.cur.executemany("""INSERT IGNORE INTO Songs(SongName, SongArtist, SpotifyID, SpotifyUrl, SpotifyUri) VALUES( %s, %s, %s, %s, %s)""", songList)
+        self.cur.executemany(
+            """INSERT IGNORE INTO Songs(SongName, SongArtist, SpotifyID, SpotifyUrl, SpotifyUri) VALUES( %s, %s, %s, %s, %s)""",
+            songList)
 
     def get_songs(self):
         self.cur.execute("SELECT SongId, SongName, SongArtist, SpotifyID, SpotifyUrl, SpotifyUri from Songs")
@@ -77,6 +83,7 @@ def home():
         users = db.list_users()
         return jsonify(users)
 
+
 @app.route('/api/Songs/', methods=['GET', 'POST'])
 def getSongs():
     db = Database()
@@ -99,6 +106,7 @@ def getSongs():
         db.con.commit()
         return jsonify(data)
 
+
 @app.route('/api/Song/', methods=['POST'])
 def insertSong():
     db = Database()
@@ -110,7 +118,8 @@ def insertSong():
     uri = payload['spotifyUri']
     db.insert_song(name, artist, spotifyId, spotifyUrl, uri)
     db.con.commit()
-    return jsonify(name=name, artist=artist, spotifyId=spotifyId, spotifyUrl=spotifyUrl, uri= uri)
+    return jsonify(name=name, artist=artist, spotifyId=spotifyId, spotifyUrl=spotifyUrl, uri=uri)
+
 
 ####################### SPOTIFY ######################################
 @app.route('/api/Spotify/search/', methods=['POST'])
@@ -120,7 +129,7 @@ def search():
     search_url = 'https://api.spotify.com/v1/search'
     search_txt = payload['search_text']
     if search_txt == '':
-        search_txt = request.args.get('search_text','')
+        search_txt = request.args.get('search_text', '')
 
     headers = {
         'Accept': 'application/json',
@@ -138,6 +147,7 @@ def search():
     song = show_songs(songs)
     return jsonify(song)
 
+
 @app.route('/api/Spotify/playlist/', methods=['POST'])
 def playlist():
     payload = request.get_json()
@@ -147,7 +157,7 @@ def playlist():
     # search_txt = '37i9dQZF1DXcBWIGoYBM5M'
     playlist_url += search_txt
     if search_txt == '':
-        search_txt = request.args.get('search_text','')
+        search_txt = request.args.get('search_text', '')
 
     headers = {
         'Accept': 'application/json',
@@ -158,7 +168,7 @@ def playlist():
         ('type', 'track'),
         ('limit', 100)
     )
-    
+
     results = {}
     i = 0
     response = requests.get(playlist_url, headers=headers, params=params).json()
@@ -173,11 +183,13 @@ def playlist():
         nextList = response['next']
     return jsonify(playlistName, results)
 
+
 def spotify_authenticate(spotify_client_id, spotify_client_secret):
     data = {'grant_type': 'client_credentials'}
     url = 'https://accounts.spotify.com/api/token'
     response = requests.post(url, data=data, auth=(spotify_client_id, spotify_client_secret))
     return response.json()['access_token']
+
 
 def show_songs(tracks):
     songs = {}
@@ -188,8 +200,9 @@ def show_songs(tracks):
         uri = item["uri"]
         id = item["id"]
         songs[i] = {"song": song, "artist": artist, "url": ext_url['spotify'], "id": id, "uri": uri}
-    
+
     return songs
+
 
 def show_playlist(tracks, results, i):
     songs = {}
@@ -200,10 +213,11 @@ def show_playlist(tracks, results, i):
         ext_url = it["external_urls"]
         id = it["id"]
         uri = it["uri"]
-        songs[i] = {"song": song, "artist": artist, "url": ext_url['spotify'], "id": id, "uri":uri}
+        songs[i] = {"song": song, "artist": artist, "url": ext_url['spotify'], "id": id, "uri": uri}
         i = i + 1
     results.update(songs)
     return i
+
 
 ####################### YOUTUBE ######################################
 
@@ -215,62 +229,84 @@ SCOPES = ['https://www.googleapis.com/auth/youtube']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
 
-
-app.secret_key = b'_5#1L"F4Q9zcmcxas]/'
+# app.secret_key = b'_5#1L"F4Q9zcmcxas]/'
 
 
 @app.route('/api/Youtube/Auth/', methods=['POST'])
 def ytAuth():
+    global youtube
 
-    if 'credentials' not in flask.session:
-        return flask.redirect('authorize')
+    # if 'credentials' not in flask.session:
+    #   return flask.redirect('authorize')
 
-    credentials = google.oauth2.credentials.Credentials(
-        **flask.session['credentials'])
+    # credentials = google.oauth2.credentials.Credentials(
+    #   **flask.session['credentials'])
 
-    youtube = googleapiclient.discovery.build(
-        API_SERVICE_NAME, API_VERSION, credentials=credentials)
+    # youtube = googleapiclient.discovery.build(
+    #    API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
-    files = youtube.files().list().execute()
+    # files = youtube.files().list().execute()
 
+    # flask.session['credentials'] = credentials_to_dict(credentials)
+
+    # return jsonify(**files)
+
+    db = Database()
     payload = request.get_json()
-    flask.session['credentials'] = credentials_to_dict(credentials, payload['id'])
+    userId = payload['id']
+    youtube = yt.auth(userId)
+    if youtube is not None:
+        db.set_auth(userId)
+    auth = db.get_user(userId)
+    return jsonify(auth)
 
-    return jsonify(**files)
 
-    # db = Database()
+# @app.route('/api/Youtube/Auth/authorize', methods=['GET'])
+# def authorize():
+#     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
+#
+#     flow.redirect_uri = 'http://localhost:3000'
+#     flow.redirect_uri = 'oauth2callback'
+#     authorization_url, state = flow.authorization_url()
+#     flask.session['state'] = state
+#
+# return jsonify(authorization_url)
+
+# @app.route('/oauth2callback')
+# def oauth2callback():
+#     Specify the state when creating the flow in the callback so that it can
+    # verified in the authorization server response.
+    # state = flask.session['state']
+    #
+    # flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+    #     CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    # flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+    #
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    # authorization_response = flask.request.url
+    # flow.fetch_token(authorization_response=authorization_response)
+    #
+    # Store credentials in the session.
+    # ACTION ITEM: In a production app, you likely want to save these
+    #              credentials in a persistent database instead.
+    # credentials = flow.credentials
+    # flask.session['credentials'] = credentials_to_dict(credentials)
+    #
     # payload = request.get_json()
     # userId = payload['id']
-    # youtube = yt.auth(userId)
-    # if youtube is not None:
-    #    db.set_auth(userId)
-    # auth = db.get_user(userId)
-    # return jsonify(auth)
-
-@app.route('/api/Youtube/Auth/authorize', methods=['GET'])
-def authorize():
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
-
-    flow.redirect_uri = 'http://localhost:3000'
-    authorization_url, state = flow.authorization_url()
-    flask.session['state'] = state
-
-    return jsonify(authorization_url)
+    # with open(f'./userauth/{userId}.pickle', 'wb') as token:
+    #     pickle.dump(credentials, token)
+    #
+    # return flask.redirect(flask.url_for('ytAuth'))
 
 
-def credentials_to_dict(credentials, userId):
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        'secret/client_secret.json', SCOPES)
-    creds = flow.run_local_server()
-    with open(f'./userauth/{userId}.pickle', 'wb') as token:
-        pickle.dump(creds, token)
-
-    return {'token': credentials.token,
-            'refresh_token': credentials.refresh_token,
-            'token_uri': credentials.token_uri,
-            'client_id': credentials.client_id,
-            'client_secret': credentials.client_secret,
-            'scopes': credentials.scopes}
+# def credentials_to_dict(credentials):
+#     return {'token': credentials.token,
+#             'refresh_token': credentials.refresh_token,
+#             'token_uri': credentials.token_uri,
+#             'client_id': credentials.client_id,
+#             'client_secret': credentials.client_secret,
+#             'scopes': credentials.scopes}
 
 
 @app.route('/api/Youtube/Playlist/Create/', methods=['POST'])
@@ -279,10 +315,11 @@ def ytCreate():
     payload = request.get_json()
     playlistName = payload['playlistName']
     userId = payload['userId']
-    youtube =  yt.auth(userId)
+    youtube = yt.auth(userId)
     playlistId = yt.create_user_playlist(playlistName, youtube)
     return jsonify(playlistId)
     # return jsonify("test")
+
 
 @app.route('/api/Youtube/Playlist/Insert/', methods=['POST'])
 def ytInsert():
@@ -292,10 +329,11 @@ def ytInsert():
     playlistId = payload['playlistId']
     songName = payload['songName']
     userId = payload['userId']
-    youtube =  yt.auth(userId)
+    youtube = yt.auth(userId)
     inserted = yt.insert_video_user_playlist(playlistId, songName, youtube)
     return jsonify(inserted)
     # return jsonify("test")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
